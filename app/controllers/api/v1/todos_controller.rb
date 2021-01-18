65a722f0247fb592ec4c6a1c2aa6d7d5271dbcdd
shownola <swasieko@gmail.com>
2021-01-18 13:21:36 -0700

    Amend todos controller

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,11 +1,17 @@
-class TodosController < ApplicationController
+class Api::V1::TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   # GET /todos
   def index
     @todos = Todo.all
+    limit = params[:_limit]
 
-    render json: @todos
+    if limit.present?
+      limit = limit.to_i
+      @todos = @todos.last(limit)
+    end
+
+    render json: @todos.reverse
   end
 
   # GET /todos/1
@@ -46,6 +52,6 @@ class TodosController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def todo_params
-      params.require(:todo).permit(:title, :completed)
+      params.require(:todo).permit(:id, :title, :completed, :_limit)
     end
 end
